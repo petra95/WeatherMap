0f0d804162f14dd9362fb4bcd700c5c24dd3fa97
petra95 <antalicspetra@gmail.com>
2016-07-25 16:14:58 +0200

    city, image, degree

--- WeatherTitan/app/src/main/java/weather/titans/p92rdi/com/weathertitan/MainWeather.java
+++ WeatherTitan/app/src/main/java/weather/titans/p92rdi/com/weathertitan/MainWeather.java
@@ -2,6 +2,9 @@ package weather.titans.p92rdi.com.weathertitan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainWeather extends AppCompatActivity {
 
@@ -10,4 +13,7 @@ public class MainWeather extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_weather);
     }
+
 }
+
+
